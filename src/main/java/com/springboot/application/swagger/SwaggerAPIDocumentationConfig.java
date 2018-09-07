9aca2dda62cb40249859581b1bfd064382a7cdb0
waqaslatif87 <waqaslatif_88@yahoo.com>
2018-09-07 20:34:47 +0300

    Update SwaggerAPIDocumentationConfig.java
    update the base package to include only the application API.

--- src/main/java/com/springboot/application/swagger/SwaggerAPIDocumentationConfig.java
+++ src/main/java/com/springboot/application/swagger/SwaggerAPIDocumentationConfig.java
@@ -28,7 +28,7 @@ public class SwaggerAPIDocumentationConfig {
 	@Bean
 	public Docket configureControllerPackageAndConvertors() {
 		return new Docket(DocumentationType.SWAGGER_2).select()
-				.apis(RequestHandlerSelectors.basePackage("com.springboot.application")).build()
+				.apis(RequestHandlerSelectors.basePackage("com.springboot.application.controller")).build()
 				.directModelSubstitute(org.joda.time.LocalDate.class, java.sql.Date.class)
 				.directModelSubstitute(org.joda.time.DateTime.class, java.util.Date.class).apiInfo(apiInfo());
 	}
