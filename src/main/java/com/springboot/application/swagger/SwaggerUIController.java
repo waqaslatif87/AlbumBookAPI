e98f72d5a6e61301b10afd811cc7d4b18ad24355
waqaslatif87 <waqaslatif_88@yahoo.com>
2018-09-07 20:40:20 +0300

    Update SwaggerUIController.java

--- src/main/java/com/springboot/application/swagger/SwaggerUIController.java
+++ src/main/java/com/springboot/application/swagger/SwaggerUIController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 public class SwaggerUIController {
-	@RequestMapping(value = "/")
+	@RequestMapping(value = "/swagger")
 	public String index() {
 		return "redirect:swagger-ui.html";
 	}
